401a93885528b2d7c46a64a60596c1afeb9242fc
Sam Callahan <callahan.l.sam@gmail.com>
2019-09-04 14:24:34 -0500

    added foxcc, help framework, and dev command

--- meleebot.py
+++ meleebot.py
@@ -13,7 +13,13 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+        await message.channel.send(msg)
+    if message.content.startswith('!dev'):
+        await message.channel.send("brought to you by cagliostro9")
+    if message.content.startswith('!foxcc'):
+        await message.channel.send('https://drive.google.com/open?id=1VLO23ZVPmUcleKMuzJpSs71muvqoCUNiXIpxXZ5GXqk')
+    if message.content.startswith('!help'):
+        await message.author.send('help goes here eventually')
 
 @client.event
 async def on_ready():
