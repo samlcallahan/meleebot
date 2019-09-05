abbbf043f1657a65e8b2cdee1e584249166c1637
Sam Callahan <callahan.l.sam@gmail.com>
2019-09-05 16:11:55 -0500

    Switched from a multitude of if statements to using the commands structure provided by discord.py

--- meleebot.py
+++ meleebot.py
@@ -4,29 +4,28 @@ from discord.ext import commands
 
 TOKEN_FILE = open("token.txt","r")
 TOKEN = TOKEN_FILE.read()
-client = discord.Client()
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+description = " A bot to serve technical documentation and frame data about Super Smash Bros. Melee to Discord users."
+bot = commands.Bot(command_prefix='!',description=description)
 
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await message.channel.send(msg)
-    if message.content.startswith('!dev'):
-        await message.channel.send("brought to you by cagliostro9")
-    if message.content.startswith('!foxcc'):
-        await message.channel.send('https://drive.google.com/open?id=1VLO23ZVPmUcleKMuzJpSs71muvqoCUNiXIpxXZ5GXqk')
-    if message.content.startswith('!help'):
-        await message.author.send('help goes here eventually')
-
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
-client.run(TOKEN)
+@bot.command()
+async def hello(ctx):
+    msg = 'Hello {0.author.mention}'.format(ctx.message)
+    await ctx.send(msg)
+
+@bot.command(aliases=["fox_cc"])
+async def foxcc(ctx):
+    await ctx.send("https://drive.google.com/open?id=1VLO23ZVPmUcleKMuzJpSs71muvqoCUNiXIpxXZ5GXqk")
+
+@bot.command(aliases=["author","creator"])
+async def dev(ctx):
+    await ctx.send("Brought to you by cagliostro9")
+
+bot.run(TOKEN)
